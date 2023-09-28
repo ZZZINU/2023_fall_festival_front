b88d89ca8c43396a8bbac6723d40b99f21e22372
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2023-09-28 21:22:52 +0900

    :sparkles: Feat: 부스 페이지 - 지도 위 건물별 마커 설정 및 클릭 이벤트 적용

--- src/pages/booth/Booth.jsx
+++ src/pages/booth/Booth.jsx
@@ -2,12 +2,23 @@ import React, { useState } from "react";
 import * as S from "./style";
 import PageTitle from "../../components/common/pageTitle/PageTitle";
 import DateSelector from "../../components/common/dateSelector/dateSelector";
+import Marker from "../../components/common/booth/marker/Marker";
+
+// 이미지
+import MapImg from "../../assets/images/map.png";
 
 function Booth() {
   const [selectedDate11, setSelectedDate11] = useState(true);
+  const [markerStates, setMarkerStates] = useState("");
 
+  // 날짜 클릭
   const handleDateClick = bool => {
     setSelectedDate11(bool);
+  };
+
+  // 각 마커 클릭
+  const handleMarkerClick = markerName => {
+    setMarkerStates(markerName);
   };
 
   return (
@@ -17,6 +28,89 @@ function Booth() {
         selectedDate11={selectedDate11}
         handleDateClick={handleDateClick}
       />
+      <S.MapWrap>
+        <S.MapBox>
+          <S.MapImg src={MapImg} alt="맵 이미지" />
+          <S.MapBoxContent>핀을 눌러 정보를 확인하세요!</S.MapBoxContent>
+
+          {/* 사회과학관 */}
+          <Marker
+            markerName={"사회과학관"}
+            markerStates={markerStates}
+            handleMarkerClick={handleMarkerClick}
+            clickaftertop={"140px"}
+            clickbeforetop={"150px"}
+            clickafterleft={"37px"}
+            clickbeforeleft={"47px"}
+          />
+
+          {/* 혜화관 */}
+          <Marker
+            markerName={"혜화관"}
+            markerStates={markerStates}
+            handleMarkerClick={handleMarkerClick}
+            clickaftertop={"103px"}
+            clickbeforetop={"113px"}
+            clickafterleft={"47px"}
+            clickbeforeleft={"55px"}
+          />
+
+          {/* 팔정도 */}
+          <Marker
+            markerName={"팔정도"}
+            markerStates={markerStates}
+            handleMarkerClick={handleMarkerClick}
+            clickaftertop={"60px"}
+            clickbeforetop={"70px"}
+            clickafterleft={"140px"}
+            clickbeforeleft={"147px"}
+          />
+
+          {/* 원흥관 */}
+          <Marker
+            markerName={"원흥관"}
+            markerStates={markerStates}
+            handleMarkerClick={handleMarkerClick}
+            clickaftertop={"30px"}
+            clickbeforetop={"40px"}
+            clickafterleft={"185px"}
+            clickbeforeleft={"193px"}
+          />
+
+          {/* 학생회관 */}
+          <Marker
+            markerName={"학생회관"}
+            markerStates={markerStates}
+            handleMarkerClick={handleMarkerClick}
+            clickaftertop={"35px"}
+            clickbeforetop={"45px"}
+            clickafterleft={"250px"}
+            clickbeforeleft={"260px"}
+          />
+
+          {/* 만해광장 */}
+          <Marker
+            markerName={"만해광장"}
+            markerStates={markerStates}
+            handleMarkerClick={handleMarkerClick}
+            clickaftertop={"70px"}
+            clickbeforetop={"80px"}
+            clickafterleft={"215px"}
+            clickbeforeleft={"225px"}
+          />
+
+          {/* 학림관 */}
+          <Marker
+            markerName={"학림관"}
+            markerStates={markerStates}
+            handleMarkerClick={handleMarkerClick}
+            clickaftertop={"100px"}
+            clickbeforetop={"110px"}
+            clickafterleft={"243px"}
+            clickbeforeleft={"250px"}
+          />
+        </S.MapBox>
+      </S.MapWrap>
     </>
   );
 }
--- src/pages/booth/style.jsx
+++ src/pages/booth/style.jsx
@@ -1,1 +1,34 @@
 import styled from "styled-components";
+
+export const MapWrap = styled.div`
+  margin-top: 20px;
+`;
+
+export const MapBox = styled.div`
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 360px;
+  height: 273px;
+  border-radius: 35px;
+  background: rgba(255, 255, 255, 0.7);
+`;
+
+export const MapImg = styled.img`
+  display: flex;
+  width: 346px;
+  height: 203px;
+`;
+
+export const MapBoxContent = styled.div`
+  display: flex;
+  color: #553c2e;
+  text-align: center;
+  font-family: Dongguk;
+  font-size: 10px;
+  font-weight: 400;
+  line-height: normal;
+  margin-top: 15px;
+`;
