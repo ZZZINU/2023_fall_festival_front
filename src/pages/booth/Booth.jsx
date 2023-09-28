075388e2dd33c4c8b717355d6fb957888a5026c6
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2023-09-28 16:50:41 +0900

    :sparkles: Feat: 날짜 selector 컴포넌트 생성

--- src/pages/booth/Booth.jsx
+++ src/pages/booth/Booth.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./style";
+import PageTitle from "../../components/common/pageTitle/PageTitle";
+import DateSelector from "../../components/common/dateSelector/dateSelector";
+
 function Booth() {
-  return <div>Booth</div>;
+  const [selectedDate11, setSelectedDate11] = useState(true);
+
+  const handleDateClick = bool => {
+    setSelectedDate11(bool);
+  };
+
+  return (
+    <>
+      <PageTitle mainTitle={"부스 정보"} />
+      <DateSelector
+        selectedDate11={selectedDate11}
+        handleDateClick={handleDateClick}
+      />
+    </>
+  );
 }
 
 export default Booth;
