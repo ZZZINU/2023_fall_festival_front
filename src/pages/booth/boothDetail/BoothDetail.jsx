4da64aff8087e0cd389bcfa530aed77cde51b1ad
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2023-09-29 19:23:33 +0900

    :sparkles: Feat: 부스 디테일 페이지 - 더미 데이터 추가

--- src/pages/booth/boothDetail/BoothDetail.jsx
+++ src/pages/booth/boothDetail/BoothDetail.jsx
@@ -12,18 +12,39 @@ import PinImg from "../../../../public/booth/boothDetailPin.png";
 
 function BoothDetail() {
   const { id } = useParams();
+
+  // 데이터
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const contentData = {
+      id: 1,
+      name: "부스타이틀",
+      description:
+        "부스에 대한 소개를 서술합니다...부스에 대한 소개를 서술합니다...부스에 대한 소개를 서술합니다...",
+      type: "부스종류",
+      location: "장소입니다",
+      thumbnail: "asd",
+      images: ["asd", "asd"],
+      is_liked: true,
+      like_cnt: 123,
+      during: "2023.09.30 06:00~18:00"
+    };
+    setData(contentData);
+  }, []);
+
   return (
     <>
       <S.BoothDetailWrap>
-        <S.BoothDetailTitle>부스 타이틀</S.BoothDetailTitle>
+        <S.BoothDetailTitle>{data.name}</S.BoothDetailTitle>
         <S.BoothDetailImage src={BoothImg} alt="부스 이미지" />
-        <S.BoothDetailRibbon>부스종류</S.BoothDetailRibbon>
+        <S.BoothDetailRibbon>{data.type}</S.BoothDetailRibbon>
 
         <S.BoothDetailFunctionWrap>
           {/* 하트 */}
           <S.BoothDetailHeartWrap>
             <S.BoothDetailHeart src={HeartImgFill} alt="채워진 하트 이미지" />
-            <S.BoothDetailHeartNum>124</S.BoothDetailHeartNum>
+            <S.BoothDetailHeartNum>{data.like_cnt}</S.BoothDetailHeartNum>
           </S.BoothDetailHeartWrap>
 
           {/* swiper */}
@@ -46,20 +67,16 @@ function BoothDetail() {
         <S.BoothDetailContentWrap>
           <S.BoothDetailContentDateWrap>
             <S.BoothDetailContentImage src={CheckMarkImg} alt="체크 이미지" />
-            <S.BoothDetailContentDate>
-              2023.09.22 18:00~23:00
-            </S.BoothDetailContentDate>
+            <S.BoothDetailContentDate>{data.during}</S.BoothDetailContentDate>
           </S.BoothDetailContentDateWrap>
 
           <S.BoothDetailContentLocationWrap>
             <S.BoothDetailContentImage src={PinImg} alt="위치 이미지" />
             <S.BoothDetailContentLocation>
-              장소입니다
+              {data.location}
             </S.BoothDetailContentLocation>
           </S.BoothDetailContentLocationWrap>
-          <S.BoothDetailContent>
-            부스에 대한 소개를 서술합니다...부스에 대한 소개를 서술합니다...
-          </S.BoothDetailContent>
+          <S.BoothDetailContent>{data.description}</S.BoothDetailContent>
         </S.BoothDetailContentWrap>
       </S.BoothDetailWrap>
     </>
