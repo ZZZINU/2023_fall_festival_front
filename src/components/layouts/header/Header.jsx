772eae5069b33d1fbc327f4c19f3683f39db640a
심서현 <simseohyun@simseohyeon-ui-MacBookAir.local>
2023-09-30 13:09:28 +0900

    :sparkles: Feat: sideBar Layout

--- src/components/layouts/header/Header.jsx
+++ src/components/layouts/header/Header.jsx
@@ -1,23 +1,46 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import * as S from "./style";
 import { Link } from "react-router-dom";
 
 function Header() {
+  const sideBarRef = useRef();
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
+
+  function openSideBar() {
+    if (isSideBarOpen) {
+      setIsSideBarOpen(false);
+    } else {
+      setIsSideBarOpen(true);
+    }
+  }
+  useEffect(() => {
+    if (isSideBarOpen) {
+      sideBarRef.current.style.display = "flex";
+    } else {
+      sideBarRef.current.style.display = "none";
+    }
+  }, [isSideBarOpen]);
   return (
     <S.HeaderWrapper>
       <S.HeaderContent>
-        <>야단법썩</>
-        <img src="./navBar/icon_open.png"></img>
+        <>야단법석</>
+        {isSideBarOpen ? (
+          <img onClick={openSideBar} src="./navBar/icon_close.png"></img>
+        ) : (
+          <img onClick={openSideBar} src="./navBar/icon_open.png"></img>
+        )}
       </S.HeaderContent>
 
-      <S.SideBarWrapper>
+      <S.SideBarWrapper ref={sideBarRef}>
         <img src="./navBar/line.png" />
-        <Link to="/">메인</Link>
-        <Link to="/notice">공지</Link>
-        <Link to="/timeLine">타임라인</Link>
-        <Link to="/booths">부스</Link>
-        <Link to="/guestBook">방명록</Link>
-        <Link to="/about">어바웃</Link>
+        <S.SideBarContent>
+          <Link to="/">메인</Link>
+          <Link to="/notice">공지</Link>
+          <Link to="/timeLine">타임라인</Link>
+          <Link to="/booths">부스</Link>
+          <Link to="/guestBook">방명록</Link>
+          <Link to="/about">어바웃</Link>
+        </S.SideBarContent>
       </S.SideBarWrapper>
     </S.HeaderWrapper>
   );
--- src/components/layouts/header/style.jsx
+++ src/components/layouts/header/style.jsx
@@ -13,7 +13,7 @@ export const HeaderWrapper = styled.div`
   max-width: 420px;
   height: 60px;
 
-  padding: 10px;
+  padding: 10px 30px;
   /* 배경 그라디언트 */
 
   background-image: url("/background.svg");
@@ -21,9 +21,7 @@ export const HeaderWrapper = styled.div`
   background-repeat: repeat;
 
   font-family: Dongguk;
-  * {
-    color: white;
-  }
+  color: white;
 `;
 
 export const HeaderContent = styled.div`
@@ -33,11 +31,30 @@ export const HeaderContent = styled.div`
 `;
 
 export const SideBarWrapper = styled.div`
-  position: relative;
+  width: 314px;
+  height: 570px;
+  top: 60px;
+  right: 0px;
+  position: absolute;
   display: none;
+  justify-content: center;
+  align-items: center;
   flex-direction: column;
+  border-radius: 20px;
+  background: linear-gradient(14deg, #ffb4d8 1.4%, #ffce9c 100%);
 
   img {
     position: absolute;
   }
+`;
+
+export const SideBarContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  height: 340px;
+  font-size: 24px;
+  color: ${props => props.theme.colors.fontBrown};
+  z-index: 9999;
 `;
