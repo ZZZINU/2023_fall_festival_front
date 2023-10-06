05957b0c919f48b943674a0cc4b69031562ebd19
Ohchanju <ohchanju3@naver.com>
2023-10-06 16:53:59 +0900

    :lipstick: Style: 메인페이지 방명록 컴포넌트 props로 반응형

--- src/components/common/guestBook/style.jsx
+++ src/components/common/guestBook/style.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const GuestBookBox = styled.div`
   width: 100%;
   padding: 15px 15px 30px 15px;
-
+  height: ${props => props.Height};
   border-radius: 10px;
   background-color: #fffaea;
   color: ${props => props.theme.colors.fontBrown};
