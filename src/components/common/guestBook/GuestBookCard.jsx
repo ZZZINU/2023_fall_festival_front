b48318fd95851992355fa0a41e3e0565ed60e12f
Ohchanju <ohchanju3@naver.com>
2023-10-07 16:41:41 +0900

    :lipstick: Style: 메인페이지 guestbook 크기 조정

--- src/components/common/guestBook/GuestBookCard.jsx
+++ src/components/common/guestBook/GuestBookCard.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import * as S from "./style";
 
 function GuestBookCard(props) {
-  const { icon, content, marginBottom, Height } = props;
+  const { icon, content, marginBottom, Height, width, isMobile } = props;
 
   return (
-    <S.GuestBookBox marginBottom={marginBottom} Height={Height}>
+    <S.GuestBookBox
+      marginBottom={marginBottom}
+      Height={Height}
+      width={width}
+      isMobile={isMobile}
+    >
       <S.GeustBookIcon>{icon}</S.GeustBookIcon>
-      <S.GeustBookText>{content}</S.GeustBookText>
+      <S.GeustBookText isMobile={isMobile}>{content}</S.GeustBookText>
     </S.GuestBookBox>
   );
 }
