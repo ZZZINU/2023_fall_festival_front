8b7fe41a6f72dcb42f408604ec3021d26a0c9004
kickcha <chasm030517@gmail.com>
2023-10-08 12:02:59 +0900

    GA 전체 주석

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
-import ReactGA from "react-ga4";
-import RouteChangeTracker from "../RouteChangeTracker";
+// import ReactGA from "react-ga4";
+// import RouteChangeTracker from "../RouteChangeTracker";
 
 // 구글 애널리틱스 운영서버만 적용
 if (process.env.REACT_APP_GOOGLE_ANALYTICS) {
