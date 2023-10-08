358bf8b466381914df4fd99cf9fb24d28af44a1e
kickcha <chasm030517@gmail.com>
2023-10-08 12:03:56 +0900

    GA 전체 주석

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,9 @@ import router from "./router";
 // import RouteChangeTracker from "../RouteChangeTracker";
 
 // 구글 애널리틱스 운영서버만 적용
-if (process.env.REACT_APP_GOOGLE_ANALYTICS) {
-  ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS);
-}
+// if (process.env.REACT_APP_GOOGLE_ANALYTICS) {
+//   ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS);
+// }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
