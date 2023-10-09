ccb2cc3cd4bd190e869c37db6d98242095ea397c
kickcha <chasm030517@gmail.com>
2023-10-10 01:57:28 +0900

    🎨Feat: 헤더 고정

--- src/components/layouts/header/style.jsx
+++ src/components/layouts/header/style.jsx
@@ -1,25 +1,19 @@
 import styled from "styled-components";
 
 export const HeaderWrapper = styled.div`
-  position: fixed;
+  position: absolute;
   display: flex;
   justify-content: center;
   align-items: center;
-
   z-index: 100;
-
   top: 0;
   width: 100%;
   max-width: 420px;
   height: 60px;
-
   padding: 10px 30px;
-  /* 배경 그라디언트 */
-
-  background-image: url("/background.svg");
+  background-image: url(/background.svg);
   background-size: 100%;
   background-repeat: repeat;
-
   font-family: Dongguk;
   color: white;
 `;
