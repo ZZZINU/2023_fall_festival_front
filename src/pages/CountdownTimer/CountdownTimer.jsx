bfc8f35335769d9efb7d88d2353cbf08341191c4
kickcha <chasm030517@gmail.com>
2023-10-10 23:51:31 +0900

    🎨Feat: 카운트다운삭제

--- src/pages/CountdownTimer/CountdownTimer.jsx
+++ src/pages/CountdownTimer/CountdownTimer.jsx
@@ -5,47 +5,25 @@ import Main from "../main/Main";
 import Header from "../../components/layouts/header/Header";
 
 const CountdownTimer = () => {
-  const [isTimeUp, setIsTimeUp] = useState(false); // timeup 여부를 추적하는 상태 변수
+  // const [isTimeUp, setIsTimeUp] = useState(false); // timeup 여부를 추적하는 상태 변수
 
-  // 시간이 종료될 때 호출되는 콜백 함수
-  const handleTimeUp = () => {
-    // console.log("Time's up ⏳");
-    setIsTimeUp(true); // timeup 상태를 true로 업데이트
-  };
+  // // 시간이 종료될 때 호출되는 콜백 함수
+  // const handleTimeUp = () => {
+  //   // console.log("Time's up ⏳");
+  //   setIsTimeUp(true); // timeup 상태를 true로 업데이트
+  // };
 
-  useEffect(() => {
-    // 특정 디데이 날짜와 현재 시간을 비교하여 디데이가 되면 자동으로 timeup
-    const targetDate = new Date("2023-10-10 18:00:00").getTime();
-    const currentDate = new Date().getTime();
+  // useEffect(() => {
+  //   // 특정 디데이 날짜와 현재 시간을 비교하여 디데이가 되면 자동으로 timeup
+  //   const targetDate = new Date("2023-10-10 18:00:00").getTime();
+  //   const currentDate = new Date().getTime();
 
-    if (currentDate >= targetDate) {
-      handleTimeUp();
-    }
-  }, []);
+  //   if (currentDate >= targetDate) {
+  //     handleTimeUp();
+  //   }
+  // }, []);
 
   // timeup이 아직 발생하지 않았을 때
-  if (!isTimeUp) {
-    return (
-      <S.Wrapper>
-        <S.Container>
-          <S.SegmentTitle>
-            축제 사이트 <br />
-            OPEN
-          </S.SegmentTitle>
-
-          <FlipCountdown
-            hideYear
-            hideMonth
-            hideDay
-            size="medium"
-            theme="light"
-            endAt={"2023-10-10 18:00:00"} // Date/Time
-            onTimeUp={handleTimeUp}
-          />
-        </S.Container>
-      </S.Wrapper>
-    );
-  }
 
   // timeup이 발생한 경우 Main 컴포넌트를 렌더링
   return (
