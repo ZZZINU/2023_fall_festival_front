23ddfe7aea760bc67ef11fbe33089fef34494e20
kickcha <chasm030517@gmail.com>
2023-10-11 11:23:02 +0900

    실시간 주석

--- src/components/timeLine/realtimeBar/RealtimeBar.jsx
+++ src/components/timeLine/realtimeBar/RealtimeBar.jsx
@@ -1,8 +1,44 @@
+// import React, { useEffect, useState } from "react";
+// import * as S from "./style";
+
+// export default function RealtimeBar({ realtimeList }) {
+//   // 실시간 공연 정보 3초 단위로 띄움-----------------------------
+
+//   const [currentIndex, setCurrentIndex] = useState(0);
+
+//   useEffect(() => {
+//     const intervalId = setInterval(() => {
+//       setCurrentIndex(prevIndex => (prevIndex + 1) % realtimeList.length);
+//     }, 3000);
+
+//     return () => {
+//       clearInterval(intervalId);
+//     };
+//   }, []);
+
+//   return (
+//     <S.TimeTableNav>
+//       <S.TimeTableSubTxt isseveral={realtimeList.length}>
+//         <img src="/timetable/timetable_logo.png" alt="Logo" />
+//         실시간
+//       </S.TimeTableSubTxt>
+//       <S.TimeTableMainTxt isseveral={realtimeList.length}>
+//         {realtimeList.length !== 0
+//           ? realtimeList[currentIndex].title
+//           : "실시간 없음"}
+//       </S.TimeTableMainTxt>
+//       <S.TimeTableSubTxt2 isseveral={realtimeList.length}>
+//         <S.LocationIMG src="/timetable/timetable_location.png" alt="Logo" />
+//         {realtimeList.length !== 0 ? realtimeList[currentIndex].place : "미정"}
+//       </S.TimeTableSubTxt2>
+//     </S.TimeTableNav>
+//   );
+// }
+
 import React, { useEffect, useState } from "react";
 import * as S from "./style";
 
 export default function RealtimeBar({ realtimeList }) {
-  // 실시간 공연 정보 3초 단위로 띄움-----------------------------
   const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
@@ -13,7 +49,7 @@ export default function RealtimeBar({ realtimeList }) {
     return () => {
       clearInterval(intervalId);
     };
-  }, []);
+  }, [realtimeList]);
 
   return (
     <S.TimeTableNav>
@@ -23,12 +59,12 @@ export default function RealtimeBar({ realtimeList }) {
       </S.TimeTableSubTxt>
       <S.TimeTableMainTxt isseveral={realtimeList.length}>
         {realtimeList.length !== 0
-          ? realtimeList[currentIndex].title
+          ? realtimeList[currentIndex]?.title
           : "실시간 없음"}
       </S.TimeTableMainTxt>
       <S.TimeTableSubTxt2 isseveral={realtimeList.length}>
         <S.LocationIMG src="/timetable/timetable_location.png" alt="Logo" />
-        {realtimeList.length !== 0 ? realtimeList[currentIndex].place : "미정"}
+        {realtimeList.length !== 0 ? realtimeList[currentIndex]?.place : "미정"}
       </S.TimeTableSubTxt2>
     </S.TimeTableNav>
   );
